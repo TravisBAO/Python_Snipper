04779d2cd717a65bcd54b9b50ef676dbeb10f745
Travis Bao <travisbao@outlook.com>
2022-11-30 18:29:28 -0500

    add script to collect plots together

--- BrawlStars/BarCharts.py
+++ BrawlStars/BarCharts.py
@@ -8,35 +8,48 @@ def bar_chart_by_trophies():
     df = pandas.read_csv('BrawlStars.csv')
     fig = px.bar(df, x="Name", y="Trophies", title="Hero's Trophies",
                  hover_data=['Rank'], color="Trophies")
-    fig.show()
+    # fig.show() # for debug
+    return fig
 
 
 def bar_chart_by_hero_parity():
     df = pandas.read_csv('BrawlStars.csv')
     fig = px.bar(df, x="Hero Rarity", y="Trophies", title="Hero's Trophies", barmode='group', color="Trophies",
                  hover_data=['Rank', 'Name'])
-    fig.show()
+    # fig.show() # for debug
+    return fig
 
 
 def bar_chart_by_hero_class():
     df = pandas.read_csv('BrawlStars.csv')
     fig = px.bar(df, x="Hero Class", y="Trophies", title="Hero's Trophies", barmode='group',
                  color="Trophies", hover_data=['Rank', 'Name'])
-    fig.show()
+    # fig.show() # for debug
+    return fig
 
 
 def histogram_chart_by_hero_parity():
     df = pandas.read_csv('BrawlStars.csv')
     fig = px.histogram(df, x="Hero Rarity", y="Trophies", title="Hero's Trophies", barmode='group',
                        color="Trophies")
-    fig.show()
+    # fig.show() # for debug
+    return fig
 
 
 def histogram_chart_by_hero_class():
     df = pandas.read_csv('BrawlStars.csv')
     fig = px.histogram(df, x="Hero Class", y="Trophies", title="Hero's Trophies", barmode='group',
                        color="Trophies", histfunc='max')
-    fig.show()
+    # fig.show() # for debug
+    return fig
+
+
+def charts_collection():
+    with open('graph.html', 'a') as charts:
+        # include_plotlyjs='cdn' only in the first figure and include_plotlyjs=False in subsequent.
+        # No need to load javascript multiple times
+        charts.write(bar_chart_by_trophies().to_html(full_html=False, include_plotlyjs='cdn'))
+        charts.write(bar_chart_by_hero_parity().to_html(full_html=False, include_plotlyjs='cdn'))
 
 
 if __name__ == "__main__":
@@ -45,7 +58,8 @@ if __name__ == "__main__":
     # bar_chart_by_trophies()
     # bar_chart_by_hero_parity()
     # bar_chart_by_hero_class()
-
-    histogram_chart_by_hero_class()
+    # histogram_chart_by_hero_class()
     # histogram_chart_by_hero_parity()
+
+    charts_collection()
 
