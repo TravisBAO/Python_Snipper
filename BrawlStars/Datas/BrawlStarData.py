b0e6a31b35581a8684bc91b46e13a1c877b82db1
Travis Bao <travisbao@outlook.com>
2022-12-03 12:04:22 -0500

    put all functions of data to a specific file

--- BrawlStars/Datas/BrawlStarData.py
+++ BrawlStars/Datas/BrawlStarData.py
@@ -1,8 +1,3 @@
-import csv
-import os.path
-from datetime import datetime
-
-import mysql.connector
 
 # database tables
 db_table_TRAVIS_HERO_LIST = ("Name", "Hero_Rarity", "Hero_Class")
@@ -115,89 +110,11 @@ TRAVIS_HERO_LIST = [
 TRAVIS_HERO_FINAL = []
 
 
-def import_data_to_csv():
-    """
-    import data to csv;
-    data source: HERO_PARAS, TRAVIS_HERO_LIST
-    @return:
-    """
-    if os.path.exists("BrawlStars.csv"):
-        os.remove("BrawlStars.csv")
-    with open("BrawlStars.csv", "a") as BrawlStarsFile:
-        writer = csv.writer(BrawlStarsFile)
-        writer.writerow(HERO_PARAS)
-        writer.writerows(TRAVIS_HERO_LIST)
 
 
-def import_data_to_csv(database, table):
-    mydb = connect_to_database(database)
-    mycursor = mydb.cursor()
-    sql = "SELECT * from" + " " + table
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+#
+# if __name__ == '__main__':
 
-    if os.path.exists("BrawlStars.csv"):
-        os.remove("BrawlStars.csv")
-    with open("BrawlStars.csv", "a") as BrawlStarsFile:
-        writer = csv.writer(BrawlStarsFile)
-        writer.writerow(HERO_PARAS)
-        for x in myresult:
-            writer.writerow(x)
-
-
-def connect_to_database(database):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="Travis",
-        password="20030421",
-        database=database)
-    return mydb
-
-
-def insert_data_to_database(database, table):
-    # connect to database
-    mydb = connect_to_database(database)
-    mycursor = mydb.cursor()
-    if table == "travis_hero_list":
-        for each_hero in TRAVIS_HERO_LIST:
-            sql = "INSERT INTO " + table + " " + str(db_table_TRAVIS_HERO_LIST).replace("'", "") + " " \
-                  + "VALUES (%s, %s, %s)"
-            val = (each_hero[0], each_hero[5], each_hero[6])  # Name, Hero_Rarity, Hero_Class
-            mycursor.execute(sql, val)
-            mydb.commit()
-    elif table == "hero_historical_data":
-        for each_hero in TRAVIS_HERO_LIST:
-            sql = "INSERT INTO " + table + " " + str(db_table_hero_historical_data).replace("'", "") + " " \
-                  + "VALUES (%s, %s, %s, %s, %s, %s)"
-            val = (each_hero[0], each_hero[1], each_hero[2], each_hero[3], each_hero[4],
-                   datetime.now().strftime('%Y-%m-%d')
-                   )  # Name,Trophies,Power_Level,Power_Points,Rank,Date
-            mycursor.execute(sql, val)
-            mydb.commit()
-    else:
-        pass
-
-
-def update_data_to_database(database, table):
-    """
-    This function is to update the dedicated key-value "Unlocked" in table travis_hero_list
-    @param database:
-    @param table:
-    @return:
-    """
-    # connect to database
-    mydb = connect_to_database(database)
-    mycursor = mydb.cursor()
-
-    for each_hero in TRAVIS_HERO_LIST:
-        sql = "UPDATE" + " " + table + " " + "SET" + " " + "Unlocked" + "=" + "true" \
-              + " " + "WHERE" + " " + "Name" + "=" + "(%s)"
-        val = (each_hero[0],)
-        mycursor.execute(sql, val)
-        mydb.commit()
-
-
-if __name__ == '__main__':
     # insert data to tables
     # insert_data_to_database("brawlstars", "travis_hero_list")
     # insert_data_to_database("brawlstars", "hero_historical_data")
@@ -206,4 +123,4 @@ if __name__ == '__main__':
     # update_data_to_database("brawlstars", "travis_hero_list")
     # update_data_to_database("brawlstars", "hero_historical_data")
 
-    import_data_to_csv("brawlstars", "hero_historical_data")
+    # import_data_to_csv("brawlstars", "hero_historical_data")
