18f4d7afc957f4a9960d516c278434e5b5f2b391
Travis Bao <travisbao@outlook.com>
2022-12-03 12:03:37 -0500

    refactor the dash of charts scripts

--- BrawlStars/MiscCharts/BarCharts.py
+++ BrawlStars/MiscCharts/BarCharts.py
@@ -44,14 +44,6 @@ def histogram_chart_by_hero_class():
     return fig
 
 
-def charts_collection():
-    with open('graph.html', 'a') as charts:
-        # include_plotlyjs='cdn' only in the first figure and include_plotlyjs=False in subsequent.
-        # No need to load javascript multiple times
-        charts.write(bar_chart_by_trophies().to_html(full_html=False, include_plotlyjs='cdn'))
-        charts.write(bar_chart_by_hero_parity().to_html(full_html=False, include_plotlyjs='cdn'))
-
-
 if __name__ == "__main__":
     # import_data_to_csv()
     import_data_to_csv("brawlstars", "hero_historical_data")
